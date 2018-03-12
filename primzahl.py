6b5d67c4eb64f255293d36eb359ddde2d55f512d
x-peter-alexander-x <171wita04@o365.htl-leoben.at>
2018-03-12 16:18:01 +0100

    Checker funktioniert

--- primzahl.py
+++ primzahl.py
@@ -1,3 +1,21 @@
 #primzahl
-#Funktion: chech if given number is prime
-#Wetter heute: bewölkt
+#Funktion: check if given number is prime
+
+def isPrime(n):
+# Logik einbauen
+    if(n <= 0):
+        return False
+    for p in range(2,n):
+        if(n % p == 0):
+            return False
+
+    return True
+
+print("Primzahlen-Checker")
+z = input("Bitte Zahl eingeben!")
+z = int(z)
+
+if(isPrime(z)):
+    print("ist eine Primzahl")
+else:
+    print("ist keine Primzahl")
