a3ca9392b0866fcadb4c21f282dfa5753d7ac908
x-peter-alexander-x <171wita04@o365.htl-leoben.at>
2018-03-12 14:41:17 +0100

    variable

--- variable.py
+++ variable.py
@@ -1,20 +1,17 @@
 #Variable
 a = 10
 b = 20
+summe = zahl+zahl2
 
-summe = a+b
-
-
-
-
+aktion = input("Welche Funktion? +/-/*/:")
+print("Funktion",aktion,"wird ausgeführt")
 
 
 
+zahl = input("Zahl 1?")
+zahl2 = input("Zahl 2 ?")
 
+zahl = int(zahl)
+zahl = int(zahl2)
 
-
-
-
-
-
-print("Summe von",a,"+",b,"=", summe)
+print("Summe von",zahl,aktion,zahl2,"=", summe)
