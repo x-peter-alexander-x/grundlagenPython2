760ad836400beeab1266232e18cd16779bd9f917
x-peter-alexander-x <171wita04@o365.htl-leoben.at>
2018-03-12 15:04:23 +0100

    Variable

--- variable.py
+++ variable.py
@@ -1,17 +1,29 @@
 #Variable
 a = 10
 b = 20
-summe = zahl+zahl2
 
 aktion = input("Welche Funktion? +/-/*/:")
 print("Funktion",aktion,"wird ausgeführt")
 
 
-
 zahl = input("Zahl 1?")
 zahl2 = input("Zahl 2 ?")
 
-zahl = int(zahl)
-zahl = int(zahl2)
 
-print("Summe von",zahl,aktion,zahl2,"=", summe)
+
+zahl = int(zahl)
+zahl2 = int(zahl2)
+
+summe = zahl+zahl2
+differenz = zahl - zahl2
+multiplikation = zahl*zahl2
+division = zahl/zahl2
+
+if (aktion == "+"):
+    print("Summe von",zahl,"+",zahl2,"=", summe)
+if (aktion == "-"):
+    print("Differenz von",zahl,"-",zahl2,"=", differenz)
+if (aktion == "*"):
+    print("Multiplikation von",zahl,"*",zahl2,"=", multiplikation)
+if (aktion == ":"):
+    print("Division von",zahl,":",zahl2,"=", division)
